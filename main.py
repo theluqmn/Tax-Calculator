3ec90059c1fda45dbd93c35b5408a5c517995e6a
luqmanity <luqman.yazrie@gmail.com>
2024-01-31 10:42:04 +0800

    it works

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ if salary > 0:
     for i in tax_bracket["single"]:
         tax_tier = i["tier"]
         tax_rate = i["rate"]
-        tax_min = i["min"]
-        tax_max = i["max"]
+        tax_min = i["min"] / 100
+        tax_max = i["max"] / 100
         if salary >= tax_min and salary < tax_max:
             tax = carry * i["rate"]
             print(f"You belong in bracket {tax_tier}. Total tax due: {tax}")
             break
         else:
             due = tax_max * tax_rate
-            carry -= i["max"]
+            carry -= tax_max
             tax += due
-            print(f"You're not in bracket {i['tier']}, {due} is paid in this bracket according to rate and {carry} is left for next bracket calculation.")
+            print(f"You're not in bracket {tax_tier}, {due} is paid in this bracket according to rate and {carry} is left for next bracket calculation.")
 else:
     print("lmao you're broke")
