0ab81021fb8428ea10cdee8616eec2b40d8a1fda
luqmanity <luqman.yazrie@gmail.com>
2024-01-31 12:00:13 +0800

    functional main code

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 from scripts import load, tools
 import time
 
-tools.Clear()
-# Loading all the brackets
-list = load.find_brackets()
-for item in list:
-    details = load.open_bracket(item)
-    name = details["name"]
-    country = details["country"]
-    print(f"ID: {item} || Bracket: {name} | country: {country} | tiers: {len(details['tiers'])}")
-
-bracket = input("Enter ID of bracket")
-
-time.sleep(5)
-
-tools.Clear()
+#Essential variables
 tax = 0
+bracket = None
+
+#Bracket Selection
+while True:
+    tools.Clear()
+    # Loading all the brackets
+    list = load.find_brackets()
+    for item in list:
+        details = load.open_bracket(item)
+        name = details["name"]
+        country = details["country"]
+        print(f"ID: {item} || Bracket: {name} | Country: {country} | Tiers: {len(details['tiers'])}\n")
+
+    #Select an ID
+    selection = input("Enter ID of bracket: ")
+
+    if selection in list:
+        bracket = load.open_bracket(selection)
+        break
+    else:
+        print(f"{selection} does not exist!")
+
+tools.Clear()
+print(f"Proceeding with {bracket['name']}")
 salary = int(input("Enter your salary (yearly): "))
 carry = salary #For calculatiosn above bracket 1
 
 if salary > 0:
-    for i in tax_bracket["tiers"]:
+    for i in bracket["tiers"]:
         tax_tier = i["tier"]
-        tax_rate = i["rate"]
-        tax_min = i["min"] / 100
-        tax_max = i["max"] / 100
+        tax_rate = i["rate"] / 100
+        tax_min = i["min"]
+        tax_max = i["max"]
         if salary >= tax_min and salary < tax_max:
             tax = carry * i["rate"]
             print(f"You belong in bracket {tax_tier}. Total tax due: {tax}")
